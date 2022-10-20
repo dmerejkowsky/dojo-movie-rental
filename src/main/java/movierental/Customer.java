93b1edb7a689780ba1e8f88bd944e4f0e9e18641
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-10-20 15:12:16 +0200

    Duplicate getFrequentRenterPoints

--- src/main/java/movierental/Customer.java
+++ src/main/java/movierental/Customer.java
@@ -46,5 +46,15 @@ public class Customer {
     return result;
   }
 
+  int getFrequentRenterPoints(Rental rental) {
+    int frequentRenterPoints = 0;
 
+    // add frequent renter points
+    frequentRenterPoints++;
+    // add bonus for a two day new release rental
+    if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
+      frequentRenterPoints++;
+
+    return frequentRenterPoints;
+  }
 }
