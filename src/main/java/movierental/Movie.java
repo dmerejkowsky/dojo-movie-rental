48f20969c9b7ef462ef4df00b5fa822adf31b39d
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-10-20 15:25:57 +0200

    Introduce static Movie.getCharge()

--- src/main/java/movierental/Movie.java
+++ src/main/java/movierental/Movie.java
@@ -14,13 +14,32 @@ public class Movie {
     _priceCode = priceCode;
   }
 
+  static double getCharge(Movie movie, int _daysRented) {
+    double charge = 0;
+    switch (movie.getPriceCode()) {
+      case Movie.REGULAR:
+        charge += 2;
+        if (_daysRented > 2)
+          charge += (_daysRented - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        charge += _daysRented * 3;
+        break;
+      case Movie.CHILDRENS:
+        charge += 1.5;
+        if (_daysRented > 3)
+          charge += (_daysRented - 3) * 1.5;
+        break;
+    }
+    return charge;
+  }
+
   public int getPriceCode() {
     return _priceCode;
   }
-  
+
   public String getTitle() {
     return _title;
   }
-
 
 }
